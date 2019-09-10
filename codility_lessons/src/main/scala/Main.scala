87445e6fc52580c75310b59d9dd0cb13fb91c117
Shunsuke Hida <kizai07@gmail.com>
2019-09-11 00:06:04 +0900

    Triangle

--- codility_lessons/src/main/scala/Main.scala
+++ codility_lessons/src/main/scala/Main.scala
@@ -416,17 +416,6 @@ object Solution {
     }
     -1
   }
-
-  def solutionTriangle(a: Array[Int]): Int = {
-    val sortedA = a.sorted
-    var count = 0
-    for (index <- (0 until sortedA.length - 2)) {
-      if (sortedA(index) + sortedA(index + 1) > sortedA(index + 2)) {
-        count += 1
-      }
-    }
-    count
-  }
 }
 
 object Main extends App {
--- codility_lessons/src/main/scala/solution/Triangle.scala
+++ codility_lessons/src/main/scala/solution/Triangle.scala
@@ -2,13 +2,19 @@ package solution
 
 object Triangle {
   def solution(a: Array[Int]): Int = {
+    if (a.length == 3) { println(a.mkString(","))}
     val sortedA = a.sorted
-    var count = 0
     for (index <- (0 until sortedA.length - 2)) {
-      if (sortedA(index) + sortedA(index + 1) >= sortedA(index + 2)) {
-        count += 1
+      if (0 < sortedA(index)) {
+        val (p, q, r) = (sortedA(index), sortedA(index + 1), sortedA(index + 2))
+        if (p == q && q == r) {
+          return 1
+        }
+        if (p + q > r) {
+          return 1
+        }
       }
     }
-    count
+    0
   }
 }
