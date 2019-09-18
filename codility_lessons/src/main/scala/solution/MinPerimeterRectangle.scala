f23c9951768cc639121591cab34e4ec22fc755a6
Shunsuke Hida <kizai07@gmail.com>
2019-09-19 07:48:01 +0900

    Add MinPerimeterRectangle

--- codility_lessons/src/main/scala/solution/MinPerimeterRectangle.scala
+++ codility_lessons/src/main/scala/solution/MinPerimeterRectangle.scala
@@ -2,6 +2,29 @@ package solution
 
 object MinPerimeterRectangle {
   def solution(n: Int): Int = {
-    22
+    import scala.collection.mutable
+
+    def isPrime(x: Int): Boolean = {
+      for (i <- 2 to Math.sqrt(x).toInt + 1) {
+        if (n % i == 0) { return false }
+      }
+      true
+    }
+    if (isPrime(n)) { return (n + 1) * 2 }
+
+    var maxCandidate = n / 2
+    var check = 1
+    var factors: mutable.Seq[Int] = mutable.Seq.empty
+
+    while (check < maxCandidate) {
+      check += 1
+      if (n % check == 0) {
+        print(s"${check}, ")
+        factors = factors :+ (if (n / check == check) { check * 4 } else { (check + n / check) * 2 })
+        maxCandidate = n / check - 1
+      }
+    }
+
+    factors.min
   }
 }
--- codility_lessons/src/test/scala/solution/MinPerimeterRectangleTest.scala
+++ codility_lessons/src/test/scala/solution/MinPerimeterRectangleTest.scala
@@ -6,4 +6,44 @@ class MinPerimeterRectangleTest extends FunSuite {
   test("testSolution1") {
     assert(MinPerimeterRectangle.solution(30) == 22)
   }
+
+  test("testSolution2") {
+    assert(MinPerimeterRectangle.solution(1) == 4)
+  }
+
+  test("testSolution3") {
+    assert(MinPerimeterRectangle.solution(36) == 24)
+  }
+
+  test("testSolution4") {
+    assert(MinPerimeterRectangle.solution(48) == 28)
+  }
+
+  test("testSolution5") {
+    assert(MinPerimeterRectangle.solution(101) == 204)
+  }
+
+  test("testSolution6") {
+    assert(MinPerimeterRectangle.solution(1234) == 1238)
+  }
+
+  test("testSolution7") {
+    assert(MinPerimeterRectangle.solution(4564320) == 8552)
+  }
+
+  test("testSolution8") {
+    assert(MinPerimeterRectangle.solution(15486451) == 30972904)
+  }
+
+  test("testSolution9") {
+    assert(MinPerimeterRectangle.solution(100000000) == 40000)
+  }
+
+  test("testSolution10") {
+    assert(MinPerimeterRectangle.solution(982451653) == 1964903308)
+  }
+
+  test("testSolution11") {
+    assert(MinPerimeterRectangle.solution(1000000000) == 126500)
+  }
 }
